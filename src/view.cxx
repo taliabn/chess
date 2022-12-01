0e5d197253d00470d609fdd8e449b80a31f4f3f0
JustinAronson <justinaronson@gmail.com>
2022-12-01 16:17:32 -0600

    Added sprites into view

--- src/view.cxx
+++ src/view.cxx
@@ -26,6 +26,42 @@ View::draw(ge211::Sprite_set& set)
             set.add_sprite(light_square_sprite, board_to_screen(pos));
         }
     }
+
+    for (auto pos : model_.board()) {
+        const char* piece_type = model_.piece_type_at(pos);
+        ge211::Image_sprite piece_sprite_
+            (ge211::Image_sprite("./Resources/white_pawn.png"));
+
+        if (strcmp(piece_type, "Pawn") == 0) {
+             piece_sprite_(model_[pos] == Player::light ?
+                           ge211::Image_sprite("./Resources/white_pawn.png") :
+                           ge211::Image_sprite("./Resources/dark_pawn.png"));
+        } else if (strcmp(piece_type, "Knight") == 0) {
+            piece_sprite_(model_[pos] == Player::light ?
+                          ge211::Image_sprite("./Resources/white_knight.png") :
+                          ge211::Image_sprite("./Resources/dark_knight.png"));
+        } else if (strcmp(piece_type, "Bishop") == 0) {
+            piece_sprite_(model_[pos] == Player::light ?
+                          ge211::Image_sprite("./Resources/white_bishop.png") :
+                          ge211::Image_sprite("./Resources/dark_bishop.png"));
+        } else if (strcmp(piece_type, "Rook") == 0) {
+            piece_sprite_(model_[pos] == Player::light ?
+                          ge211::Image_sprite("./Resources/white_rook.png") :
+                          ge211::Image_sprite("./Resources/dark_rook.png"));
+        } else if (strcmp(piece_type, "Queen") == 0) {
+            piece_sprite_(model_[pos] == Player::light ?
+                          ge211::Image_sprite("./Resources/white_queen.png") :
+                          ge211::Image_sprite("./Resources/dark_queen.png"));
+        } else if (strcmp(piece_type, "King") == 0) {
+            piece_sprite_(model_[pos] == Player::light ?
+                          ge211::Image_sprite("./Resources/white_king.png") :
+                          ge211::Image_sprite("./Resources/dark_king.png"));
+        } else {
+            continue;
+        }
+
+        set.add_sprite(piece_sprite_, board_to_screen(pos), 1);
+    }
 }
 
 View::Position
